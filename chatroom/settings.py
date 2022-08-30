fab117ccb3b3d599bd25f5174160f03853ac3938
techonair <techionmail@gmail.com>
2022-08-30 17:09:56 +0530

    Ready for Heroku

--- chatroom/settings.py
+++ chatroom/settings.py
@@ -12,10 +12,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 ## Requirement.txt creation command ##
 python -m  pipreqs.pipreqs --encoding utf-8  E:\ProjectName
 """
-
-
-from decouple import config
-
+import django_heroku
+import os
+import environ
+environ.Env.read_env('.env')
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
+DEBUG = os.environ['DEBUG']
 
 ALLOWED_HOSTS = []
 
@@ -129,3 +129,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+if os.getcwd() == '/app':
+    DEBUG = False
+
+django_heroku.settings(locals())
